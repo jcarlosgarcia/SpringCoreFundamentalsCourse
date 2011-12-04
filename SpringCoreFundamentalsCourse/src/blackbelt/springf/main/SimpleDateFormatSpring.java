1b47075fe3bb3b447850607c4cf57abdda5f9e7c
jcgperez <jcgperez@jcgperez-fedora16>
2011-12-04 16:45:02 +0100

    SimpleDateFormat Spring

--- SpringCoreFundamentalsCourse/src/blackbelt/springf/main/SimpleDateFormatSpring.java
+++ SpringCoreFundamentalsCourse/src/blackbelt/springf/main/SimpleDateFormatSpring.java
@@ -3,14 +3,18 @@ package blackbelt.springf.main;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class SimpleDateFormatSpring {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat sdf = (SimpleDateFormat) ac.getBean("euDateFormat");
 		String formattedDate = sdf.format(new Date());
 		
 		System.out.print("formattedDate: " + formattedDate);
